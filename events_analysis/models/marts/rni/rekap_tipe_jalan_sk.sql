8ac3922ae7f1f83fabba798dc0d1a434e4c4bd2b
Hannan Azinuddin <hazinuddin@gmail.com>
2026-02-19 12:11:58 +0700

    Add rekap tipe jalan RNI

---
+++ events_analysis/models/marts/rni/rekap_tipe_jalan_sk.sql
@@ -0,0 +1,22 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=['LINKID', 'YEAR'],
+        incremental_strategy='delete+insert'
+    )
+}}
+
+select
+sde.gdb_util.next_rowid({{"'"+this.schema+"'"}}, {{"'"+this.name+"'"}}) as OBJECTID,
+rni.LINKID,
+lrs.SK_LENGTH as TOTAL_LENGTH,
+(ROAD_TYPE_1*(lrs.SK_LENGTH/TOTAL_LENGTH)) as ROAD_TYPE_1,
+(ROAD_TYPE_2*(lrs.SK_LENGTH/TOTAL_LENGTH)) as ROAD_TYPE_2,
+(ROAD_TYPE_3*(lrs.SK_LENGTH/TOTAL_LENGTH)) as ROAD_TYPE_3,
+(ROAD_TYPE_4*(lrs.SK_LENGTH/TOTAL_LENGTH)) as ROAD_TYPE_4,
+(ROAD_TYPE_5*(lrs.SK_LENGTH/TOTAL_LENGTH)) as ROAD_TYPE_5,
+YEAR as YEAR,
+CURRENT_TIMESTAMP as UPDATE_DATE    
+from (select * from {{ref("rekap_tipe_jalan")}} where year = {{var('year')}}) rni
+left join {{ref('active_lrs')}} lrs 
+on rni.LINKID = lrs.LINKID
