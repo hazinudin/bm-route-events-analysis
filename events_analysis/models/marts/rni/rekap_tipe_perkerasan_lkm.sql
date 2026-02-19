f824bfecbf6c92a56fd5aab295ec0d07d29d762a
Hannan Azinuddin <hazinuddin@gmail.com>
2026-02-19 12:46:55 +0700

    Add rekap tipe perkerasan LKM RNI

---
+++ events_analysis/models/marts/rni/rekap_tipe_perkerasan_lkm.sql
@@ -0,0 +1,36 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=['LINKID', 'YEAR'],
+        incremental_strategy='delete+insert'
+    )
+}}
+
+{%- set routes = var('routes', none) -%}
+
+SELECT 
+sde.gdb_util.next_rowid({{"'"+this.schema+"'"}}, {{"'"+this.name+"'"}}) as OBJECTID,
+t1.LINKID, 
+SUM(t1.SEGMENT_LENGTH) AS TOTAL_LENGTH, 
+SUM(CASE WHEN t1.SURF_TYPE IN (3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20) THEN t1.SEGMENT_LENGTH ELSE 0 END) AS ASPAL, 
+SUM(CASE WHEN t1.SURF_TYPE IN (21) THEN t1.SEGMENT_LENGTH ELSE 0 END) AS RIGID, 
+SUM(CASE WHEN t1.SURF_TYPE IN (1, 2) THEN t1.SEGMENT_LENGTH ELSE 0 END) AS TANAH,
+{{var('year')}} as YEAR,
+CURRENT_TIMESTAMP as UPDATE_DATE
+FROM
+( 
+    SELECT LINKID, FROM_STA, TO_STA, SURF_TYPE, SEGMENT_LENGTH
+    FROM {{ref("stg_rni_combined")}}
+    {% if routes %} 
+        WHERE LINKID IN (
+            {%- for route in routes -%}
+                '{{ route }}'{%- if not loop.last -%}, {%- endif -%}
+            {%- endfor -%}
+        )
+    {%- else -%}
+        WHERE 1=1
+    {% endif %}
+    AND year = {{var('year')}}
+    AND semester = {{var('semester')}} 
+) t1 
+GROUP BY t1.LINKID
