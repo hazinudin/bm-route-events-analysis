de56f1c52871e7a7c1dd87d76327782042305072
Hannan Azinuddin <hazinuddin@gmail.com>
2026-02-25 09:34:32 +0700

    Add kemantapan IRI max model

---
+++ events_analysis/models/marts/kemantapan/kemantapan_max_iri.sql
@@ -0,0 +1,58 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=['LINKID', 'YEAR', 'SEMESTER'],
+        incremental_strategy='delete+insert'
+    )
+}}
+
+{% set rules = var('kemantapan_iri_ranges') %}
+
+SELECT
+    sde.gdb_util.next_rowid({{"'"+this.schema+"'"}}, {{"'"+this.name+"'"}}) as OBJECTID,
+    CURRENT_TIMESTAMP as UPDATE_DATE,
+    base.LINKID,
+    {{ prov_name_column() }} as BM_PROV_ID,
+    base.YEAR,
+    base.SEMESTER,
+    base.IRI,
+    base.TOTAL_LENGTH,
+    {{ satker_balai_columns() }},
+
+    {{kemantapan_columns()}}
+
+FROM
+(
+    SELECT
+        merged.LINKID as LINKID,
+
+        {{var('year')}} as YEAR,
+        {{var('semester')}} as SEMESTER,
+        SUM(SEGMENT_LENGTH) as TOTAL_LENGTH,
+        AVG(merged.IRI) as IRI,
+
+        {{ km_sum('merged', rules.paved.surf_types, none, rules.paved.ranges.good.max, 'P_GOOD_KM', none, 'IRI') }},
+        {{ km_sum('merged', rules.paved.surf_types, rules.paved.ranges.fair.min, rules.paved.ranges.fair.max, 'P_FAIR_KM', none, 'IRI') }},
+        {{ km_sum('merged', rules.paved.surf_types, rules.paved.ranges.poor.min, rules.paved.ranges.poor.max, 'P_POOR_KM', none, 'IRI') }},
+        {{ km_sum('merged', rules.paved.surf_types, rules.paved.ranges.bad.min, none, 'P_BAD_KM', none, 'IRI') }},
+
+        {{ km_sum('merged', rules.unpaved.surf_types, none, rules.unpaved.ranges.good.max, 'UP_GOOD_KM', none, 'IRI') }},
+        {{ km_sum('merged', rules.unpaved.surf_types, rules.unpaved.ranges.fair.min, rules.unpaved.ranges.fair.max, 'UP_FAIR_KM', none, 'IRI') }},
+        {{ km_sum('merged', rules.unpaved.surf_types, rules.unpaved.ranges.poor.min, rules.unpaved.ranges.poor.max, 'UP_POOR_KM', none, 'IRI') }},
+        {{ km_sum('merged', rules.unpaved.surf_types, rules.unpaved.ranges.bad.min, none, 'UP_BAD_KM', none, 'IRI') }}
+
+    FROM (
+        select
+            e.LINKID,
+            AVG(e.IRI) as IRI,
+            max(e.SURF_TYPE) as SURF_TYPE,
+            max(e.SEGMENT_LENGTH) as SEGMENT_LENGTH
+
+        from ({{ rni_iri_join(var('semester'), var('year'), var('routes', none)) }}) e
+        GROUP BY e.LINKID, e.FROM_STA, e.TO_STA
+    ) merged
+
+    GROUP BY merged.LINKID
+) base
+{{ join_satker_balai('base') }}
+{{ get_prov_name('base') }}
---
+++ events_analysis/models/marts/kemantapan/kemantapan_max_iri_sk.sql
@@ -0,0 +1,32 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key=['LINKID', 'YEAR', 'SEMESTER'],
+        incremental_strategy='delete+insert'
+    )
+}}
+
+SELECT
+    sde.gdb_util.next_rowid({{"'"+this.schema+"'"}}, {{"'"+this.name+"'"}}) as OBJECTID,
+    base.UPDATE_DATE,
+    base.LINKID,
+    base.BM_PROV_ID,
+    base.YEAR,
+    base.SEMESTER,
+    base.IRI,
+    lrs.SK_LENGTH as TOTAL_LENGTH,
+    base.SATKER_PPK_ID,
+    base.BALAI_ID,
+
+    {{ kemantapan_sk_columns('base', 'lrs') }}
+
+FROM {{ ref("kemantapan_max_iri") }} base
+LEFT JOIN {{ ref("active_lrs") }} lrs ON base.LINKID = lrs.LINKID
+WHERE base.YEAR = {{ var('year') }} AND base.SEMESTER = {{ var('semester') }}
+{% if var('routes', none) %}
+    AND base.LINKID IN (
+        {%- for route in var('routes') -%}
+            '{{ route }}'{%- if not loop.last -%}, {%- endif -%}
+        {%- endfor -%}
+    )
+{% endif %}
