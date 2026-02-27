cf67aa7ff78a5f2994f5ea85d772ff925208cca2
Hannan Azinuddin <hazinuddin@gmail.com>
2026-02-27 15:00:20 +0700

    fix(get_prov_name): Add end_date is null query to prevent duplicate

--- events_analysis/macros/get_prov_name.sql
+++ events_analysis/macros/get_prov_name.sql
@@ -1,6 +1,7 @@
 {% macro get_prov_name(source_alias, linkid_column = 'LINKID') %}
 LEFT JOIN {{ source('misc', 'MAP_PROV_ROUTE') }} mpr
-    ON {{ source_alias }}.{{ linkid_column }} = mpr.ROUTEID
+    ON {{ source_alias }}.{{ linkid_column }} = mpr.LINKID
+    AND mpr.END_DATE IS NULL
 LEFT JOIN {{ source('misc', 'REF_PROVINCE') }} rp
     ON SUBSTR({{ source_alias }}.{{ linkid_column }}, 1, 2) = rp.BM_PROV_ID
 {% endmacro %}
